f19b7be4965d4bba21d9fba30fb7c815323c5e51
Stefano Lottini <stefano.lottini@datastax.com>
2022-05-09 15:41:09 +0200

    code style in tests

--- feast_cassandra_online_store/astra_db_tests.py
+++ feast_cassandra_online_store/astra_db_tests.py
@@ -7,20 +7,20 @@ from tests.integration.feature_repos.integration_test_repo_config import (
 
 
 HERE = pathlib.Path(__file__).parent.absolute()
-config = dotenv_values(HERE / '.env')
-ASTRA_DB_SECURE_CONNECT_BUNDLE = config['ASTRA_DB_SECURE_CONNECT_BUNDLE']
-ASTRA_DB_USERNAME = config['ASTRA_DB_USERNAME']
-ASTRA_DB_PASSWORD = config['ASTRA_DB_PASSWORD']
-ASTRA_DB_KEYSPACE = config['ASTRA_DB_KEYSPACE']
+config = dotenv_values(HERE / ".env")
+ASTRA_DB_SECURE_CONNECT_BUNDLE = config["ASTRA_DB_SECURE_CONNECT_BUNDLE"]
+ASTRA_DB_USERNAME = config["ASTRA_DB_USERNAME"]
+ASTRA_DB_PASSWORD = config["ASTRA_DB_PASSWORD"]
+ASTRA_DB_KEYSPACE = config["ASTRA_DB_KEYSPACE"]
 
 
 ASTRA_DB_CONFIG = {
-    'type': ('feast_cassandra_online_store.cassandra_online_store'
-             '.CassandraOnlineStore'),
-    'secure_bundle_path': ASTRA_DB_SECURE_CONNECT_BUNDLE,
-    'username': ASTRA_DB_USERNAME,
-    'password': ASTRA_DB_PASSWORD,
-    'keyspace': ASTRA_DB_KEYSPACE,
+    "type": ("feast_cassandra_online_store.cassandra_online_store"
+             ".CassandraOnlineStore"),
+    "secure_bundle_path": ASTRA_DB_SECURE_CONNECT_BUNDLE,
+    "username": ASTRA_DB_USERNAME,
+    "password": ASTRA_DB_PASSWORD,
+    "keyspace": ASTRA_DB_KEYSPACE,
 }
 
 FULL_REPO_CONFIGS = [
--- feast_cassandra_online_store/cassandra_tests.py
+++ feast_cassandra_online_store/cassandra_tests.py
@@ -7,15 +7,15 @@ from tests.integration.feature_repos.integration_test_repo_config import (
 
 
 HERE = pathlib.Path(__file__).parent.absolute()
-config = dotenv_values(HERE / '.env')
-CASSANDRA_HOSTS = config['CASSANDRA_HOSTS']
-CASSANDRA_KEYSPACE = config['CASSANDRA_KEYSPACE']
-CASSANDRA_PORT = config.get('CASSANDRA_PORT')
-CASSANDRA_USERNAME = config.get('CASSANDRA_USERNAME')
-CASSANDRA_PASSWORD = config.get('CASSANDRA_PASSWORD')
+config = dotenv_values(HERE / ".env")
+CASSANDRA_HOSTS = config["CASSANDRA_HOSTS"]
+CASSANDRA_KEYSPACE = config["CASSANDRA_KEYSPACE"]
+CASSANDRA_PORT = config.get("CASSANDRA_PORT")
+CASSANDRA_USERNAME = config.get("CASSANDRA_USERNAME")
+CASSANDRA_PASSWORD = config.get("CASSANDRA_PASSWORD")
 
 if CASSANDRA_HOSTS:
-    hosts = CASSANDRA_HOSTS.split(',')
+    hosts = CASSANDRA_HOSTS.split(",")
 else:
     hosts = None
 if CASSANDRA_PORT:
@@ -27,13 +27,13 @@ else:
 CASSANDRA_CONFIG = {
     k: v
     for k, v in {
-        'type': ('feast_cassandra_online_store.cassandra_online_store'
-                 '.CassandraOnlineStore'),
-        'hosts': hosts,
-        'port': port,
-        'keyspace': CASSANDRA_KEYSPACE,
-        'username': CASSANDRA_USERNAME,
-        'password': CASSANDRA_PASSWORD,
+        "type": ("feast_cassandra_online_store.cassandra_online_store"
+                 ".CassandraOnlineStore"),
+        "hosts": hosts,
+        "port": port,
+        "keyspace": CASSANDRA_KEYSPACE,
+        "username": CASSANDRA_USERNAME,
+        "password": CASSANDRA_PASSWORD,
     }.items()
     if v is not None
 }
