d4394268b3a2c176ebf5d8557f1d30cd2ecc6085
Stefano Lottini <stefano.lottini@datastax.com>
2022-05-20 15:45:45 +0200

    v 0.1.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ here = pathlib.Path(__file__).parent.resolve()
 
 setup(
     name="feast-cassandra",
-    version="0.1.2",
+    version="0.1.3",
     author="Stefano Lottini",
     author_email="stefano.lottini@datastax.com",
     package_dir={"": "src"},
@@ -15,7 +15,7 @@ setup(
     #         "command=importable:function",
     #     ],
     # },
-    url="https://github.com/hemidactylus/feast-cassandra-online-store",
+    url="https://github.com/datastaxdevs/feast-cassandra-online-store",
     license="LICENSE.txt",
     description="Cassandra/Astra DB support for Feast online store",
     long_description=(here / "README.md").read_text(encoding="utf-8"),
